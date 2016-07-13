5efe4139e10380a89e54ee29d48e1cd78e950f68
iporer <raohongrui@outlook.com>
2016-07-13 21:06:58 +0800

    fix bug of day color

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 (function () {
     var today = new Date();
     var header = document.getElementsByTagName("header")[0];
-    var dayColor = ["#ff0000","#ff8000","#ffff00","#00ff00","#00ffff","#0000ff","#8000ff"];
+    var dayColor = ["#ff0000", "#ff8000", "#ffff00", "#00ff00", "#00ffff", "#0000ff", "#8000ff"];
     var day = today.getDay();
-    header.style.borderTopColor = dayColor[day-3];
+    header.style.borderTopColor = dayColor[day % 7];
 })();
