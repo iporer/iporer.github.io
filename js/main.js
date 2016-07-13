1df8cddbd1db8adeed82145abc273b5ed1e7563f
iporer <raohongrui@outlook.com>
2016-07-13 21:05:23 +0800

    set day color

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by iporer on 7/12/16.
+ */
+(function () {
+    var today = new Date();
+    var header = document.getElementsByTagName("header")[0];
+    var dayColor = ["#ff0000","#ff8000","#ffff00","#00ff00","#00ffff","#0000ff","#8000ff"];
+    var day = today.getDay();
+    header.style.borderTopColor = dayColor[day-3];
+})();
