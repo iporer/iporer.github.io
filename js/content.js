871e835f6df2650b37db1f254126d0d9f8c92de7
iporer <raohongrui@outlook.com>
2016-07-15 00:24:27 +0800

    make content scrollable

--- js/content.js
+++ js/content.js
@@ -7,12 +7,44 @@
     var recently = document.getElementById("recently");
     var categories = document.getElementById("categories");
     var tags = document.getElementById("tags");
-    if (!markdownToc) {
-        content.remove();
+    var pageLeft = document.getElementsByClassName("page-left")[0];
+    var pageRight = document.getElementsByClassName("page-right")[0];
+    if (window.screen.width < 500) { // 针对移动设备
+        pageLeft.style.display = "none";
+        pageRight.style.width = "100%";
+    }
+    window.onresize = function () {
+        content.style.width = pageLeft.offsetWidth - 20 + "px"; // 保持高度不变，20px是padding距离
+        if (document.body.clientWidth < 600) { // 针对移动设备
+            pageLeft.style.display = "none";
+            pageRight.style.width = "100%";
+        } else {
+            pageLeft.style.display = "block";
+            pageRight.style.width = "75%";
+        }
+    };
+    if (markdownToc) {
+        tags.remove();
+        categories.remove();
+        recently.remove();
+        content.appendChild(markdownToc);
+
+        // 计算content位置
+        var contentTop = content.offsetTop;
+        content.style.height = content.offsetHeight + "px"; // 保持高度不变
+        var bodyScrollTop = 0;
+
+        window.onscroll = function () {
+            bodyScrollTop = document.body.scrollTop;
+            content.style.width = pageLeft.offsetWidth - 20 + "px"; // 保持高度不变，20px是padding距离
+            if (bodyScrollTop - contentTop > -30) { // 设定content的top为30
+                content.classList.add('fixed');
+            } else {
+                content.classList.remove('fixed');
+            }
+        };
+
         return;
     }
-    tags.remove();
-    categories.remove();
-    recently.remove();
-    content.appendChild(markdownToc);
+    content.remove();
 })();
